8bd08338b3803f1474808e01f6e51a1885756c2a
luisalfarob <alfa.b14@gmail.com>
2018-01-07 13:11:37 -0300

    update code

--- 6-Recurrent_neural_network.py
+++ 6-Recurrent_neural_network.py
@@ -66,10 +66,26 @@ class BatchGenerator(object):
         self._last_batch = self._next_batch()
 
     def _next_batch(self):
-        """Generate a single batch from the current cursor position in the data."""
+        """Generate a single batch from the current cursor position in the data.
+            Example:
+                text = 'the house is a big place where sleep'
+                batch_size = 5
+                segment = len(text) // batch_size
+                cursor = [offset * segment for offset in range(batch_size)]
+                batch = np.zeros(shape=[batch_size, 27])
+                for b in range(batch_size):
+                    batch[b, char2id(text[cursor[b]])] = 1
+                    print(text[cursor[b]], )
+                    cursor[b] += 1  # the real code have overflow control
+
+                # print -> (t, 0); (s, 7); (' ', 14); (a, 21); (r, 28)
+        """
         batch = np.zeros(shape=(self._batch_size, vocabulary_size), dtype=np.float)
         for b in range(self._batch_size):
+            # map letter to one hot. This process take a letter jumping by the whole text given by cursor
+            # index value
             batch[b, char2id(self._text[self._cursor[b]])] = 1.0
+            # enable restart
             self._cursor[b] = (self._cursor[b] + 1) % self._text_size
         return batch
 
@@ -126,51 +142,58 @@ print(batches2string(train_batches.next()))
 
 num_nodes = 64
 
-graph = tf.Graph()
-with graph.as_default():
-    # Parameters:
-    # Input gate: input, old output cell, and bias.
-    ix = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
-    im = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
-    ib = tf.Variable(tf.zeros([1, num_nodes]))
-    # Forget gate: input, old output, and bias.
-    fx = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
-    fm = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
-    fb = tf.Variable(tf.zeros([1, num_nodes]))
-    # Memory cell: input, old state cell and bias.
-    cx = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
-    cm = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
-    cb = tf.Variable(tf.zeros([1, num_nodes]))
-    # Output gate: input, old output, and bias.
-    ox = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
-    om = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
-    ob = tf.Variable(tf.zeros([1, num_nodes]))
-    # Variables saving state across unrollings.
-    saved_output = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
-    saved_state = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
-    # Classifier weights and biases.
-    w = tf.Variable(tf.truncated_normal([num_nodes, vocabulary_size], -0.1, 0.1))
-    b = tf.Variable(tf.zeros([vocabulary_size]))
+# Parameters:
+# Input gate: input, old output cell, and bias.
+ix = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+im = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+ib = tf.Variable(tf.zeros([1, num_nodes]))
+# Forget gate: input, old output, and bias.
+fx = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+fm = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+fb = tf.Variable(tf.zeros([1, num_nodes]))
+# Memory cell: input, old state cell and bias.
+cx = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+cm = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+cb = tf.Variable(tf.zeros([1, num_nodes]))
+# Output gate: input, old output, and bias.
+ox = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+om = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+ob = tf.Variable(tf.zeros([1, num_nodes]))
+# Variables saving state across unrollings.
+saved_output = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
+saved_state = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
+# Classifier weights and biases.
+w = tf.Variable(tf.truncated_normal([num_nodes, vocabulary_size], -0.1, 0.1))
+b = tf.Variable(tf.zeros([vocabulary_size]))
 
 
-    def lstm_cell(i, o, state):
-        """ Create a LSTM cell. See e.g.: http://colah.github.io/posts/2015-08-Understanding-LSTMs/
+def lstm_cell(i, o, state):
+    """ Create a LSTM cell. See e.g.: http://colah.github.io/posts/2015-08-Understanding-LSTMs/
 
-        :param i: Input sample
-        :param o: Old output of the cell (h_t-1)
-        :param state: Old state of the cell (C_t-1)
-        :return: New output cell (h_t), New state cell (C_t)
-        """
-        # input gate = w_xi * i + w_hi * o + b_i
-        i_t = tf.sigmoid(tf.matmul(i, ix) + tf.matmul(o, im) + ib)
-        # forget gate = w_xf * i + w_hf * o + b_f
-        f_t = tf.sigmoid(tf.matmul(i, fx) + tf.matmul(o, fm) + fb)
-        # cell candidate = tanh(w_xc * i + w_hc * o + b_c)
-        c_tau = tf.matmul(i, cx) + tf.matmul(o, cm) + cb
-        # new cell state = forget * old_cell_state + input * cell_candidate
-        c_t = f_t * state + i_t * tf.tanh(c_tau)
-        # output gate = sigmod(w_xo * i + w_ho * o + b_o)
-        o_t = tf.sigmoid(tf.matmul(i, ox) + tf.matmul(o, om) + ob)
-        return o_t * tf.tanh(c_t), c_t
+    :param i: Input sample
+    :param o: Old output of the cell (h_t-1)
+    :param state: Old state of the cell (C_t-1)
+    :return: New output cell (h_t), New state cell (C_t)
+    """
+    # input gate = w_xi * i + w_hi * o + b_i
+    i_t = tf.sigmoid(tf.matmul(i, ix) + tf.matmul(o, im) + ib)
+    # forget gate = w_xf * i + w_hf * o + b_f
+    f_t = tf.sigmoid(tf.matmul(i, fx) + tf.matmul(o, fm) + fb)
+    # cell candidate = tanh(w_xc * i + w_hc * o + b_c)
+    c_tau = tf.matmul(i, cx) + tf.matmul(o, cm) + cb
+    # new cell state = forget * old_cell_state + input * cell_candidate
+    c_t = f_t * state + i_t * tf.tanh(c_tau)
+    # output gate = sigmod(w_xo * i + w_ho * o + b_o)
+    o_t = tf.sigmoid(tf.matmul(i, ox) + tf.matmul(o, om) + ob)
+    return o_t * tf.tanh(c_t), c_t
 
+# Input data.
+train_data = list()
+for _ in range(num_unrollings + 1):
+    train_data.append(
+        tf.placeholder(tf.float32, shape=[batch_size, vocabulary_size]))
+train_inputs = train_data[:num_unrollings]
+train_labels = train_data[1:]  # labels are inputs shifted by one time step.
 
+for b in range(_batch_size):
+    print(_text[_cursor[b]])
